a0e1a60b37c3e788e5bf832b88cce2179fa1802b
apermon <antonioperezmondejar@gmail.com>
2020-01-17 23:19:51 +0100

    funcionalidad pelicula random semanal

--- app/app.js
+++ app/app.js
@@ -1,45 +1,63 @@
-var myMoviesApp = angular.module('myMoviesApp', []);
+var myMoviesApp = angular.module('myMoviesApp', ['ngRoute']); /*ngRoute para las rutas al acceso de HOME*/
 
-myMoviesApp.controller('MoviesController', ['$scope', function($scope){
+myMoviesApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'views/home.html',
+      controller: 'MoviesController'
+    })
+    .when('/directory', {
+      templateUrl: 'views/directory.html',
+      controller: 'MoviesController'
+    }).otherwise({
+      redirectTo: '/home'
+    });
+}]);
+
+/* Creación de un nuevo directorio para elegir de forma random la pelicula de la semana*/
+myMoviesApp.directive('randomMovie', [function(){
+  return {
+    restrict: 'E', /*Solo puede ser un elemento*/
+    scope: {
+      movies: '=',
+      title: '='
+    },
+    templateUrl: 'views/random.html',
+    controller: function($scope){
+      $scope.random = Math.floor(Math.random() * 4)
+    }
+  };
+}]);
+
+myMoviesApp.controller('MoviesController', ['$scope', '$http', function($scope, $http){
 
   $scope.removeMovie = function(movie){
     var removedMovie = $scope.movies.indexOf(movie);
     $scope.movies.splice(removedMovie, 1);
-  }
+  };
 
+  $scope.addMovie = function(){
+    $scope.movies.push({
+      name: $scope.newMovie.name,
+      category: $scope.newMovie.category,
+      director: $scope.newMovie.director,
+      duration: parseInt($scope.newMovie.duration),
+      premiere: $scope.newMovie.premiere,
+      available: true
+    });
 
-  $scope.movies = [
-    {
-      name: "Star Wars: Episodio IX",
-      category: "Fantasía/Ciencia ficción",
-      director: "J. J. Abrams",
-      premiere: 2019,
-      duration: 142,
-      available: true
-    },
-    {
-      name: "Joker",
-      category: "Drama/Suspense",
-      director: "Todd Phillips",
-      premiere: 2019,
-      duration: 122,
-      available: true
-    },
-    {
-      name: "Avatar 2",
-      category: "Fantasía/Cine de ciencia ficción",
-      director: "James Cameron",
-      premiere: 2021,
-      duration: "-",
-      available: false
-    },
-    {
-      name: "Vengadores: Endgame",
-      category: "Fantasía/Cine de ciencia ficción",
-      director: "Anthony y Joe Russo",
-      premiere: 2019,
-      duration: 181,
-      available: true
-    }
-  ];
+    /* Limpia el formulario al añadir una nueva película */
+    $scope.newMovie.name = "";
+    $scope.newMovie.category = "";
+    $scope.newMovie.director = "";
+    $scope.newMovie.duration = "";
+    $scope.newMovie.premiere = "";
+  };
+
+  /* Carga de datos Json */
+   $http.get('data/movies.json').then(function(response){
+     var data = response.data;
+     $scope.movies = data;
+   });
+
 }]);
